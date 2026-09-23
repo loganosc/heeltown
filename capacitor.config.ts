d73d967e2b5295af2f2058cdb073e83efddcf482
Logan Oscher <156248522+loganosc@users.noreply.github.com>
2026-09-23 16:34:17 -0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.lovable.8da29e2215a74bf58b76bfb53a6401b3',
+  appName: 'A Lovable project',
+  webDir: 'dist',
+  server: {
+    url: 'https://8da29e22-15a7-4bf5-8b76-bfb53a6401b3.lovableproject.com?forceHideBadge=true',
+    cleartext: true
+  }
+};
+
+export default config;
